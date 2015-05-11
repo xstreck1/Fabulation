f12abdd153cdacae8c145ddacd48d2e5e7cea7ae
Adam Streck <adam.streck@gmail.com>
2015-05-12 00:05:23 +0200

    Menu and word list present

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Game : MonoBehaviour {
+    List<int> _players_points;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartGame (int players, int points, int seconds)
+    {
+        _players_points = Enumerable.Repeat(points, players).ToList();
+    }
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour {
+    GameObject _menu;
+    GameObject _game;
+
+    // Use this for initialization
+    void Start () {
+        _menu = GameObject.Find("Canvas");
+        _game = GameObject.Find("Game");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartGame()
+    {
+        int players = (int) _menu.transform.FindChild("PlayersCount").GetComponent<Slider>().value;
+        int points = (int)_menu.transform.FindChild("PointsCount").GetComponent<Slider>().value;
+        int seconds = (int)_menu.transform.FindChild("SecondsCount").GetComponent<Slider>().value;
+        _menu.SetActive(false);
+        _game.GetComponent<Game>().StartGame(players, points, seconds);
+    }
+}
