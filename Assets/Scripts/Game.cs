0b509a8b5ea5db5a1dae1fb67e2e80b8c39f1b3e
Adam Streck <adam.streck@gmail.com>
2015-05-16 17:06:02 +0200

    New game screen built.

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -20,22 +20,46 @@ public class Game : MonoBehaviour
     };
 
     Manager _manager;
-    Image _background;
-    Text _cue;
-    Text _player_name;
+    GameObject _check, _cross, _arrow;
+    Text _previous;
+    Transform[] _actors = new Transform[2]; // First judge, than player
+    Image[] _backgrounds = new Image[2];
+    Text[]  _cues = new Text[2];
+    Text[] _player_names = new Text[2];
+    Transform[] _meters = new Transform[2];
 
     List<int> _players_points;
 
-    float _time;
+    string _current_cue;
+    float _ROUND_TIME;
     float _timer;
-    int _current_player;
-    
+    int[] _actor_ids = new int[2];
+
     void Awake()
     {
         _manager = GameObject.Find("Manager").GetComponent<Manager>();
-        _background = transform.FindChild("Background").GetComponent<Image>();
-        _cue = transform.FindChild("Cue").GetComponent<Text>();
-        _player_name = transform.FindChild("PlayerName").GetComponent<Text>();
+        _actors[0] = transform.FindChild("Judge");
+        _actors[1] = transform.FindChild("Player");
+        _check = _actors[0].FindChild("Content").FindChild("Check").gameObject;
+        _cross = _actors[0].FindChild("Content").FindChild("Cross").gameObject;
+        _arrow = _actors[0].FindChild("Content").FindChild("Arrow").gameObject;
+        _arrow.SetActive(false);
+        _previous = _actors[1].FindChild("Content").FindChild("Previous").gameObject.GetComponent<Text>();
+        _previous.text = "";
+        foreach (int i in Enumerable.Range(0, 2))
+        {
+            _backgrounds[i] = _actors[i].FindChild("Background").GetComponent<Image>();
+            _player_names[i] = _actors[i].FindChild("Name").FindChild("Text").GetComponent<Text>();
+            _cues[i] = _actors[i].FindChild("Cue").FindChild("Text").GetComponent<Text>();
+            _meters[i] = _actors[i].FindChild("Timer").FindChild("Meter");
+        }
+    }
+
+    void Start()
+    {
+#if UNITY_EDITOR
+        StartGame(2, 2, 5);
+#endif
     }
 
     // Update is called once per frame
@@ -45,35 +69,72 @@ public class Game : MonoBehaviour
         if (_timer < 0)
         {
             Handheld.Vibrate();
-            _players_points[_current_player] -= 1;
-            if (_players_points[_current_player] == 0)
-            {
-                _manager.EndGame(_players_points);
-            }
-            else
-            {
-                NextPlayer();
-            }
+            _check.SetActive(false);
+            _cross.SetActive(false);
+            _arrow.SetActive(true);
+        } else
+        {
+            _meters[0].localScale = _meters[1].localScale = new Vector3((_ROUND_TIME - _timer) / _ROUND_TIME, 1, 1);
         }
     }
 
-    void NewPlayer(int new_player)
+    public void Check()
     {
-        _current_player = new_player;
-        _timer = _time;
-        _background.color = colors[_current_player];
-        _player_name.text = "Player: " + (_current_player+1).ToString();
-        _cue.text = WordList.words[Random.Range(0, WordList.words.Count())];
+        _previous.text = _current_cue + "\n" + _previous.text;
+        NextPlayer();
+    }
+
+    public void Cross()
+    {
+        _players_points[_actor_ids[1]] -= 1;
+        if (_players_points[_actor_ids[1]] == 0)
+        {
+            _manager.EndGame(_players_points);
+        }
+        else
+        {
+            NextPlayer();
+        }
+    }
+
+    public void Arrow()
+    {
+        _players_points[_actor_ids[1]] -= 1;
+        if (_players_points[_actor_ids[1]] == 0)
+        {
+            _manager.EndGame(_players_points);
+        }
+        else
+        {
+            _check.SetActive(true);
+            _cross.SetActive(true);
+            _arrow.SetActive(false);
+            NextPlayer();
+        }
+    }
+
+    void NewPlayer(int judge, int player)
+    {
+        _actor_ids[0] = judge;
+        _actor_ids[1] = player;
+        _timer = _ROUND_TIME;
+        _current_cue = WordList.words[Random.Range(0, WordList.words.Count())];
+        foreach (int i in Enumerable.Range(0, 2))
+        {
+            _backgrounds[i].color = colors[_actor_ids[i]];
+            _player_names[i].text = "Player: " + (_actor_ids[i] + 1).ToString();
+            _cues[i].text = _current_cue;
+        }
     }
 
     public void StartGame(int players, int points, int seconds)
     {
         _players_points = Enumerable.Repeat(points, players).ToList();
-        _time = seconds;
-        NewPlayer(0);
+        _ROUND_TIME = seconds;
+        NewPlayer(0,1);
     }
     public void NextPlayer()
     {
-        NewPlayer((_current_player + 1) % _players_points.Count());
+        NewPlayer((_actor_ids[0] + 1) % _players_points.Count(), (_actor_ids[1] + 1) % _players_points.Count());
     }
 }
