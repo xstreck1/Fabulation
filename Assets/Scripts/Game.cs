6b901922c9e038a4eed3055723e9af5ceffd58c5
Adam Streck <adam.streck@gmail.com>
2015-05-28 18:34:54 +0200

    Extended controls in game.

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -20,7 +20,8 @@ public class Game : MonoBehaviour
     };
 
     GameObject _check, _cross, _arrow;
-    Text _previous;
+    GameObject[] _previous = new GameObject[2];
+    Text[] _previous_text = new Text[2];
     Transform[] _actors = new Transform[2]; // First judge, than player
     Image[] _backgrounds = new Image[2];
     Text[] _cues = new Text[2];
@@ -28,9 +29,14 @@ public class Game : MonoBehaviour
     Transform[] _meters = new Transform[2];
 
     string _current_cue;
-    float _REACTIVE_DELAY = 1f; // In seconds, how long are the buttons blocked to prevent double-click.
-    float _timer;
+    readonly int _PHASE_COUNT = 3;
+    readonly float _REACTIVE_DELAY = 1f; // In seconds, how long are the buttons blocked to prevent double-click.
+    readonly float _JUDGE_TIME = 5f; // In seconds, how long the judge can decide.
+    readonly float _PREPARE_TIME = 5f; // In seconds, how long the player can prepare
+    double[] _timers = new double[2];
+    double[] _time = new double[2];
     int[] _actor_ids = new int[2]; // Current acting players
+    int _phase = 0;
 
     void Awake()
     {
@@ -38,12 +44,12 @@ public class Game : MonoBehaviour
         _actors[1] = transform.FindChild("Player");
         _check = _actors[0].FindChild("Content").FindChild("Check").gameObject;
         _cross = _actors[0].FindChild("Content").FindChild("Cross").gameObject;
-        _arrow = _actors[0].FindChild("Content").FindChild("Arrow").gameObject;
-        _arrow.SetActive(false);
-        _previous = _actors[1].FindChild("Content").FindChild("Previous").gameObject.GetComponent<Text>();
-        _previous.text = "";
+        _arrow = _actors[1].FindChild("Content").FindChild("Arrow").gameObject;
         foreach (int i in Enumerable.Range(0, 2))
         {
+            _previous[i] = _actors[i].FindChild("Content").FindChild("Previous").gameObject;
+            _previous_text[i] = _previous[i].GetComponent<Text>();
+            _previous_text[i].text = "";
             _backgrounds[i] = _actors[i].FindChild("Background").GetComponent<Image>();
             _player_names[i] = _actors[i].FindChild("Name").FindChild("Text").GetComponent<Text>();
             _cues[i] = _actors[i].FindChild("Cue").FindChild("Text").GetComponent<Text>();
@@ -55,85 +61,123 @@ public class Game : MonoBehaviour
     {
         StaticData.ResetScore();
         Debug.Log(StaticData.lists.Count);
+        _phase = 0;
         NewPlayer(0, 1);
+        NewPhase();
+    }
+
+    void NewPhase()
+    {
+        switch (_phase)
+        {
+            case 0:
+                _check.SetActive(false);
+                _cross.SetActive(false);
+                _arrow.SetActive(true);
+                _previous[0].SetActive(true);
+                _previous[1].SetActive(false);
+                foreach (int i in Enumerable.Range(0, 2))
+                {
+                    _backgrounds[i].color = colors[_actor_ids[i]];
+                    _player_names[i].text = (i == 0 ? "JUDGE" : "SPEAK") + ": Player " + (_actor_ids[i] + 1).ToString();
+                    _cues[i].text = "";
+                }
+                _timers[0] = _time[0] = double.PositiveInfinity;
+                _timers[1] = _time[1] = _PREPARE_TIME;
+                break;
+            case 1:
+                foreach (int i in Enumerable.Range(0, 2))
+                {
+                    _cues[i].text = _current_cue;
+                }
+                _timers[0] = _time[0] = double.PositiveInfinity;
+                _timers[1] = _time[1] = StaticData.seconds;
+                break;
+            case 2:
+                _check.SetActive(true);
+                _cross.SetActive(true);
+                _arrow.SetActive(false);
+                _previous[0].SetActive(false);
+                _previous[1].SetActive(true);
+                _timers[0] = _time[0] = _JUDGE_TIME;
+                _timers[1] = _time[1] = double.PositiveInfinity;
+                break;
+            case 3:
+                _cross.SetActive(false);
+                _timers[0] = _time[0] = double.PositiveInfinity;
+                _timers[1] = _time[1] = double.PositiveInfinity;
+                break;
+        }
+    }
+
+    void nextPhase()
+    {
+        _phase = _phase + 1 % _PHASE_COUNT;
+        NewPhase();
     }
 
     void Update()
     {
-        float new_time = _timer - Time.deltaTime;
-
-        if (_timer > 0)
+        foreach (int i in Enumerable.Range(0, 2))
         {
-            // Display arrow
-            if (new_time <= 0)
+            if (_timers[i] == double.PositiveInfinity)
             {
-                Handheld.Vibrate();
-                _check.SetActive(false);
-                _cross.SetActive(false);
-                _arrow.SetActive(true);
+                _meters[i].localScale = Vector3.one;
             }
-            // Change the meter
-            _meters[0].localScale = _meters[1].localScale = new Vector3((StaticData.seconds - new_time) / StaticData.seconds, 1, 1);
+            else
+            {
+                // Finish phase
+                if (_timers[i] < 0)
+                {
+                    nextPhase();
+                    Handheld.Vibrate();
+                }
+                else
+                {
+                    _timers[i] -= Time.deltaTime;
+                    // Move meter
+                    _meters[i].localScale = new Vector3((float)((_time[i] - _timers[i]) / _time[i]), 1, 1);
+                }
+            }
         }
-
-        _timer = new_time;
     }
 
     public void Check()
     {
-        // Prevent double click error
-        if (StaticData.seconds - _timer < _REACTIVE_DELAY)
+        StaticData.score[_actor_ids[1]] += 1;
+        foreach (int i in Enumerable.Range(0, 2))
         {
-            return;
+            _previous_text[i].text = _current_cue + "\n" + _previous_text[i].text;
+        }
+        if (StaticData.score[_actor_ids[1]] == StaticData.points)
+        {
+            Application.LoadLevel("Score");
         }
         else
         {
-            StaticData.score[_actor_ids[1]] += 1;
-            _previous.text = _current_cue + "\n" + _previous.text;
-            if (StaticData.score[_actor_ids[1]] == StaticData.points)
-            {
-                Application.LoadLevel("Score");
-            }
-            else
-            {
-                NextPlayer();
-            }
+            _phase = 0;
+            NextPlayer();
+            NewPhase();
         }
     }
 
     public void Cross()
     {
-        // Prevent double click error
-        if (StaticData.seconds - _timer < _REACTIVE_DELAY)
-        {
-            return;
-        }
-        else
-        {
-            NextPlayer();
-        }
+        _phase = 0;
+        NextPlayer();
+        NewPhase();
     }
 
     public void Arrow()
     {
-        _check.SetActive(true);
-        _cross.SetActive(true);
-        _arrow.SetActive(false);
-        NextPlayer();
+        nextPhase();
     }
 
     void NewPlayer(int judge, int player)
     {
         _actor_ids[0] = judge;
         _actor_ids[1] = player;
-        _timer = StaticData.seconds;
         _current_cue = WordLists.SelectWord(WordLists.SelectList(StaticData.lists)); // Get the random word
-        foreach (int i in Enumerable.Range(0, 2))
-        {
-            _backgrounds[i].color = colors[_actor_ids[i]];
-            _player_names[i].text = "Player: " + (_actor_ids[i] + 1).ToString();
-            _cues[i].text = _current_cue;
-        }
     }
 
     public void NextPlayer()
